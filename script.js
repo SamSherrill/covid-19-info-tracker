5afe1542aca0e8bc38c7d10ee18a822e861ae10f
Tucker Hembree <bladeofxp@gmail.com>
2020-03-18 10:07:41 -0400

    simply tested basic virus tracker using ajax; functionality seems to be sufficient

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
     console.log("ready!");
-});
+
+    
+
+    var queryURLTracker = "https://thevirustracker.com/free-api?"; 
+    $.ajax({
+        url: queryURLTracker,
+        dataType: 'json',
+        success: function(data) {
+          console.log(data);
+        }
+      });
+    });
