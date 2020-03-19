8e04d7db3b23bef42e9959e85bfd8b24253a6420
Tucker Hembree <bladeofxp@gmail.com>
2020-03-19 12:31:32 -0400

    updated virus-tracker.js to incorporate proxies through heroku and added .then function to last ajax in order to include recommended catch functions to receive error messages in the console. Will need to display error messages on DOM eventually

--- assets/scripts/virus-tracker.js
+++ assets/scripts/virus-tracker.js
@@ -12,64 +12,77 @@ $(document).ready(function() {
     "CN",
     "JP"
   ];
-
   // Special Note: we'll want to try to minimize the use of ajax for the sake of performance;
   // consider adding a submit button that actually runs the whole graph update, rather than dynamically
   // re-generating the DOM on each button press.
-
   // Currently, all 3 of these are running on page load, and they print based upon run speed, not order
-
-  var globalStatsQuery = "https://thevirustracker.com/free-api?global=stats";
+  var globalStatsQuery =
+    "https://cors-anywhere.herokuapp.com/https://thevirustracker.com/free-api?global=stats";
   $.ajax({
     url: globalStatsQuery,
     dataType: "json",
     method: "GET"
-  }).then(function(data) {
-    console.log(data);
-    console.log(data.stat);
-    if (data.stat === "ok") {
-      console.log("Total cases of the virus: " + data.results[0].total_cases)
-      console.log("New cases today: " + data.results[0].total_new_cases_today)
-      $("#data-test").text("Total Cases of the Virus: " + data.results[0].total_cases);
-    }
-  });
-
+  })
+    .then(function(data) {
+      console.log("==========inside globalStatsQuery ===========");
+      console.log(data);
+      console.log(data.stat);
+      if (data.stat === "ok") {
+        console.log("Total cases of the virus: " + data.results[0].total_cases);
+        console.log(
+          "New cases today: " + data.results[0].total_new_cases_today
+        );
+      }
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
   var countryStatsQuery =
-    "https://thevirustracker.com/free-api?countryTotal=" + countryCode[0];
+    "https://cors-anywhere.herokuapp.com/https://thevirustracker.com/free-api?countryTotal=" +
+    countryCode[0];
   $.ajax({
     url: countryStatsQuery,
     dataType: "json",
     method: "GET"
-  }).then(function(data) {
-    var countryInfo = data.countrydata[0];
-    console.log(data);
-    console.log("Name of this country: " + countryInfo.info.title);
-    console.log("Total cases in this country: " + countryInfo.total_cases);
-    console.log("New cases today: " + countryInfo.total_new_cases_today);
-  });
-
-  var queryURLTracker =
-    "https://thevirustracker.com/free-api?countryTimeline=" + countryCode[0];
-  $.ajax({
-    url: queryURLTracker,
-    dataType: "json",
-    success: function(data) {
-      console.log(data.timelineitems["0"]);
+  })
+    .then(function(data) {
+      console.log("==========inside countryStatsQuery ===========");
+      var countryInfo = data.countrydata[0];
       console.log(data);
-      var timelineData = data.timelineitems["0"];
-      var x = [];
-      var y = [];
-      var keys = Object.keys(timelineData);
-      var virusDuration = keys.length
-      for (i = 0; i < virusDuration - 1; i++) {
-        x.push(keys[i]);
-        y.push(timelineData[Object.keys(timelineData)[i]].total_cases);
-      }
-      console.log(x);
-      console.log(y);
-      console.log(keys.length);
-      console.log(Array.isArray(x)); 
-      console.log(Array.isArray(y)); 
+      console.log("Name of this country: " + countryInfo.info.title);
+      console.log("Total cases in this country: " + countryInfo.total_cases);
+      console.log("New cases today: " + countryInfo.total_new_cases_today);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  var countryTimelineQuery =
+    "https://cors-anywhere.herokuapp.com/https://thevirustracker.com/free-api?countryTimeline=" +
+    countryCode[0];
+  $.ajax({
+    url: countryTimelineQuery,
+    dataType: "json",
+    method: "GET"
+  }).then(function(data) {
+    console.log("==========inside countryTimelineQuery ===========");
+    console.log(data.timelineitems["0"]);
+    console.log(data);
+    var timelineData = data.timelineitems["0"];
+    var x = [];
+    var y = [];
+    var keys = Object.keys(timelineData);
+    var virusDuration = keys.length;
+    for (i = 0; i < virusDuration - 1; i++) {
+      x.push(keys[i]);
+      y.push(timelineData[Object.keys(timelineData)[i]].total_cases);
     }
+    console.log(x);
+    console.log(y);
+    console.log(keys.length);
+    console.log(Array.isArray(x));
+    console.log(Array.isArray(y));
+  })
+  .catch(function(err) {
+    console.error(err);
   });
 });
