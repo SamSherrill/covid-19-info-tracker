98537dd16e3a2f0ce26bfba0d4906a03ef404139
Tucker Hembree <bladeofxp@gmail.com>
2020-03-20 14:27:02 -0400

    graph now displays information pulled from ajax call to tracker API; added dropdown functionality; dropdown now properly selects the country displayed and changes ajax call; information from call now dynamically updates with dropdown select

--- assets/scripts/virus-tracker.js
+++ assets/scripts/virus-tracker.js
@@ -1,124 +1,77 @@
-$(document).ready(function () {
-  // Country List: USA, Iran, South Korea, Italy, UK, Germany, France, Spain, China, Japan
-  var countryCode = [
-    "US",
-    "IR",
-    "KR",
-    "IT",
-    "GB",
-    "DE",
-    "FR",
-    "ES",
-    "CN",
-    "JP"
-  ];
-  // Special Note: we'll want to try to minimize the use of ajax for the sake of performance;
-  // consider adding a submit button that actually runs the whole graph update, rather than dynamically
-  // re-generating the DOM on each button press.
-  // Currently, all 3 of these are running on page load, and they print based upon run speed, not order
-  var globalStatsQuery =
-    "https://cors-anywhere.herokuapp.com/https://thevirustracker.com/free-api?global=stats";
-  $.ajax({
-    url: globalStatsQuery,
-    dataType: "json",
-    method: "GET"
-  })
-    .then(function(data) {
-      console.log("==========inside globalStatsQuery ===========");
-      console.log(data);
-      console.log(data.stat);
-      if (data.stat === "ok") {
-        console.log("Total cases of the virus: " + data.results[0].total_cases);
-        console.log(
-          "New cases today: " + data.results[0].total_new_cases_today
-        );
-      }
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
-  var countryStatsQuery =
-    "https://cors-anywhere.herokuapp.com/https://thevirustracker.com/free-api?countryTotal=" +
-    countryCode[0];
-  $.ajax({
-    url: countryStatsQuery,
-    dataType: "json",
-    method: "GET"
-  })
-    .then(function(data) {
-      console.log("==========inside countryStatsQuery ===========");
-      var countryInfo = data.countrydata[0];
-      console.log(data);
-      console.log("Name of this country: " + countryInfo.info.title);
-      console.log("Total cases in this country: " + countryInfo.total_cases);
-      console.log("New cases today: " + countryInfo.total_new_cases_today);
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
+// Country List: USA, Iran, South Korea, Italy, UK, Germany, France, Spain, China, Japan
+
+var countryCode = ["US", "IR", "KR", "IT", "GB", "DE", "FR", "ES", "CN", "JP"]
+// var currentCountry = 0;
+// var countryTimelineQuery =
+// "https://cors-anywhere.herokuapp.com/https://thevirustracker.com/free-api?countryTimeline=" + countryCode[currentCountry];
+
+// Then call & pass in like this ---> displayCountry(countryTimelineQuery);
+// At startup that will display the USA's COVID-19 graph, but we can then change currentCountry everytime the drop down changes,
+// and re-run displayCountry(countryTimelineQuery);
+
+// $(#OUR DROP DOWN).on("state-change", function to change currentCountry)
+// call displayCountry() again
+
+$(document).ready(function() {
+  console.log("ready!");
+  displayCountry(0);
+});
+
+function displayCountry(currentCountry) {
   var countryTimelineQuery =
-    "https://cors-anywhere.herokuapp.com/https://thevirustracker.com/free-api?countryTimeline=" +
-    countryCode[0];
+    "https://cors-anywhere.herokuapp.com/https://thevirustracker.com/free-api?countryTimeline=" + countryCode[currentCountry];
   $.ajax({
-    url: countryTimelineQuery,
+    url: countryTimelineQuery, 
     dataType: "json",
     method: "GET"
-  }).then(function(data) {
-    console.log("==========inside countryTimelineQuery ===========");
-    console.log(data.timelineitems["0"]);
-    console.log(data);
-    var timelineData = data.timelineitems["0"];
-    var x = [];
-    var y = [];
-    var keys = Object.keys(timelineData);
-    var virusDuration = keys.length;
-    for (i = 0; i < virusDuration - 1; i++) {
-      x.push(keys[i]);
-      y.push(timelineData[Object.keys(timelineData)[i]].total_cases);
-    }
-    console.log(x);
-    console.log(y);
-    console.log(keys.length);
-    console.log(Array.isArray(x));
-    console.log(Array.isArray(y));
   })
-  .catch(function(err) {
-    console.error(err);
-  });
+    .then(function(data) {
+      var timelineData = data.timelineitems["0"];
+      var x = [];
+      var y = [];
+      var keys = Object.keys(timelineData);
+      var virusDuration = keys.length;
+      for (i = 0; i < virusDuration - 1; i++) {
+        x.push(keys[i]);
+        y.push(timelineData[Object.keys(timelineData)[i]].total_cases);
+      }
+      lineGrapher(x, y, data);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+}
 
-  // The chart below is shown on the <canvas> tag in index.html, commented below.
-  // <canvas id="data-space"></canvas>
-  var canvasForChart = document.getElementById("data-space").getContext("2d");
-  
-  var dataTestSetX = [];
-  var dataTestSetY = [];
-  for (i = 0; i < 21; i++) {
-    dataTestSetX.push(i);
-    dataTestSetY.push(i);
-  }
+// The chart below is shown on the <canvas> tag in index.html, commented below.
+// <canvas id="data-space"></canvas>
+var canvasForChart = document.getElementById("data-space").getContext("2d");
 
-
-
-  var myLineChart = new Chart(canvasForChart, {
+function lineGrapher(x, y, data) {
+  var countryTitle = data.countrytimelinedata[0].info.title;
+  new Chart(canvasForChart, {
     type: "line",
     data: {
-      labels: dataTestSetX,
-      datasets: [{
-        label: 'Simple Test Line',
-        data: dataTestSetY,
-        backgroundColor: 'magenta',
-        borderColor: 'orange',
-        borderWidth: 1
-      }]
+      labels: x,
+      datasets: [
+        {
+          label: countryTitle,
+          data: y,
+          fill: false,
+          borderColor: "black",
+          borderWidth: 5
+        }
+      ]
     },
     options: {
       scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
           }
-        }]
+        ]
       }
     }
   });
-});
+}
